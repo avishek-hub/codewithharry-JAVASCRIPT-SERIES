cabf50f04e6651e471ad9708795af27dd0e1e885
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-11-16 20:51:13 +0530

    Add files via upload

---
+++ codewithharry JAVASCRIPT SERIES/22.GuessTheNumber.js
@@ -0,0 +1,23 @@
+let ranNum = Math.random()
+console.log(ranNum);
+
+let userNum = prompt(parseInt("Enter any Number of your choise"))
+
+while (userNum > ranNum)  
+{
+    console.log("Your guessed number is greater than the random number")
+}
+
+{
+if (userNum < ranNum)
+{
+    console.log("Your guessed number is lesser than the random number")
+    }
+
+else  (userNum = ranNum);{
+    console.log("Your guessed number is correct")
+}
+}
+
+
+
