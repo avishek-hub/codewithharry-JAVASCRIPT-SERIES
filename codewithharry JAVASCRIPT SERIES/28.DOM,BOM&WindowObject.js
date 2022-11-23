c78c453a7aecf52c3f7b7ed1902c191fabf47fd2
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-11-23 13:32:29 +0530

    Add files via upload

---
+++ codewithharry JAVASCRIPT SERIES/28.DOM,BOM&WindowObject.js
@@ -0,0 +1,13 @@
+console.log(window)
+window.console.log(window) //Window object represents brower window and provides methods to controls it. It is a global object
+
+
+/*
+DOM-    Document Object Model
+    -Your whole HTML page is converted into a JavaScript object and named as document.
+    -In the DOM, all HTML elements are defined as objects.
+    -You can also change allmost all CSS properties from DOM javascript
+
+BOM-  Browser Object Model
+    BOM represents additional objects provided by the browser (host enviroment) for working with everything except the documnet.*/
+    
