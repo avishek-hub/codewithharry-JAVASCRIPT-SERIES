c6f74a9c3bd19d88f808f03bcbe5a56a5fd4e6d4
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-11-30 00:02:45 +0530

    Add files via upload

---
+++ codewithharry JAVASCRIPT SERIES/36.SearchingTheDOM.js
@@ -0,0 +1,8 @@
+
+//  Change the card title to red
+let ctitle = document.getElementById("firstCardTittle")
+ctitle.style.color = "red"
+
+let action = document.getElementsByClassName("dropdown")
+action.style.color = "purple"
+
