e70310262841ba99a9ecfa35ae6222281954fca1
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-11-29 16:49:25 +0530

    Add files via upload

---
+++ codewithharry JAVASCRIPT SERIES/35.TableNavigation.js
@@ -0,0 +1,8 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)  //You will get all the elements of rows in the form of HTML collections.
+
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tFoot)
+console.log(t.tBody)
